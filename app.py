d6b5f3786ea586bc0fa168d55dfaef0c6002770a
Shalin Ahasan <shalin.a.ahasan@gmail.com>
2024-06-27 12:07:04 +0100

    add ndockerfile and requirements.txt

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 
-cred =  os.getenv("GOOGLE_CREDENTIALS")
+cred = os.getenv("GOOGLE_CREDENTIALS")
 password = os.getenv("PASSWORD")
 
 @app.route("/")
@@ -11,4 +11,5 @@ def index():
     return f'{cred}\n\n{password}'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
