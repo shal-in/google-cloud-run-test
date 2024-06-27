71c10374bfc6a635ccd01e51ab98b9ace1bc58ae
Shalin Ahasan <shalin.a.ahasan@gmail.com>
2024-06-27 12:32:30 +0100

    updates

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 import os
+import json
 
 app = Flask(__name__)
 
-cred = os.getenv("GOOGLE_CREDENTIALS")
+cred_json = os.getenv("GOOGLE_CREDENTIALS")
+cred = json.loads(cred_json)
+
 password = os.getenv("PASSWORD")
 
 @app.route("/")
 def index():
-    return f'{cred}\n\n{password}'
+    return f'{cred}\n\n\n\n\n\n{password}'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
